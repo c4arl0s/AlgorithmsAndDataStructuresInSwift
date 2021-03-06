506cf02a703612e10edb4baef8965a3f6c6c0dad
c4arl0s <c.santiago.cruz@gmail.com>
2021-03-06 06:18:23 -0600

    Update links to improve project performance

--- SwiftProjects/AvoidInfiniteRecursion.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-func badSum(number: Int) -> Int {
-
-    if number == 0 {
-        return 0
-    }
-    return number + badSum(number: (number-1))
-}
-let result = badSum(number: -1)
-print(result)
--- SwiftProjects/factorial.playground/Contents.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-func factorial(number: UInt64) -> UInt64 {
-   return number < 2 ? 1 : number * factorial(number: number-1)
-}
-
-let factorialNumber = factorial(number: 3)
-print(factorialNumber)
